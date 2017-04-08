8feb0077c93320eb9ffae33e09b2f5cc3f6da23a
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-04-07 18:35:28 -0700

    users scaffold

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+  end
+
+  # PATCH/PUT /users/1
+  def update
+
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :bio, :email, :password_digest)
+    end
+end
